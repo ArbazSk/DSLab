739b89e57d74ca4a8f10243e6ec706af6011b981
user.email <shahid.the.coder@gmial.com>
2017-07-21 16:03:40 +0530

    infix postfix program

---
+++ infixpostfix.c
@@ -0,0 +1,116 @@
+#include<stdio.h>
+#include<ctype.h>
+
+typedef struct conversion
+{
+	char[30];
+	int top;
+}stack;
+
+void push(stack*,char);
+char pop(stack*);
+void convert(char[],char[]);
+int priority(char);
+int main()
+{
+	char infix[30],poxtfix[30];
+	printf("\n enter infix expression\n");
+	scanf("%s" ,infix);
+	convert(infix,postfix);
+	printf("\nPOSFIX EXPRESSION\n%s",postfix);
+	return 0;
+}
+
+void convert(char in[],char post[])
+{
+	char opr;
+	stack s1;
+	int j=0,i;
+	for(i=0;in[i]!='\0';i++)
+	{
+		if(isalpha(in[i]))
+		post[j++]=in[i];
+		if(in[i]=='(')
+		push(&s1,in[i]);
+		if(in[i]=='+'||in[i]=='-'||in[i]=='/'||in[i]=='*')
+		{
+			if(s1.top!=-1)
+			{
+				opr=pop(&s1);#include<stdio.h>
+#include<ctype.h>
+
+typedef struct conversion
+{
+	char[30];
+	int top;
+}stack;
+
+void push(stack*,char);
+char pop(stack*);
+void convert(char[],char[]);
+int priority(char);
+int main()
+{
+	char infix[30],poxtfix[30];
+	printf("\n enter infix expression\n");
+	scanf("%s" ,infix);
+	convert(infix,postfix);
+	printf("\nPOSFIX EXPRESSION\n%s",postfix);
+	return 0;
+}
+
+void convert(char in[],char post[])
+{
+	char opr;
+	stack s1;
+	int j=0,i;
+	for(i=0;in[i]!='\0';i++)
+	{
+		if(isalpha(in[i]))
+		post[j++]=in[i];
+		if(in[i]=='(')
+		push(&s1,in[i]);
+		if(in[i]=='+'||in[i]=='-'||in[i]=='/'||in[i]=='*')
+		{
+			if(s1.top!=-1)
+			{
+				opr=pop(&s1);
+				while(priority(opr)>=priority(in[i]))
+				{
+					post[j++]=opr;
+					opr=pop(&s1);
+				}
+				push(&s1,)
+
+				while(priority(opr)>=priority(in[i]))
+				{
+					post[j++]=opr;
+					opr=pop(&s1);
+				}
+				push(&s1,opr);
+				push(&s1,in[i]);
+			}
+			else
+			pushpush(&s1,in[i]);
+		}
+		if(in[i]==')')
+		{
+			opr=pop(&s1);
+			while(opr!='(')
+			{
+				post[j++]=opr;
+				opr=pop(&s1);
+			}
+		}
+	}
+	while(s1.top!=-1)
+	{
+		post[j++]=pop(&s1);
+		post[j]='\0';
+	}
+	
+int priority(char);
+{
+	if(char==$)
+	return 3;
+	else if(
