485d9f7b922520d74c9f38f3c84fecee612aa1b6
user.email <shahid.the.coder@gmial.com>
2017-07-28 14:53:03 +0530

    infix to postfix program

--- infixpostfix.c
+++ infixpostfix.c
@@ -1,23 +1,27 @@
 #include<stdio.h>
-#include<ctype.h>
+#include<ctype.h> //for isaplha() function
 
 typedef struct conversion
 {
-	char[30];
+	char a[30];
 	int top;
 }stack;
 
 void push(stack*,char);
+
 char pop(stack*);
+
 void convert(char[],char[]);
+
 int priority(char);
+
 int main()
 {
-	char infix[30],poxtfix[30];
-	printf("\n enter infix expression\n");
-	scanf("%s" ,infix);
+	char infix[30],postfix[30];
+	printf("\nENTER INFIX EXPRESSION\n");
+	scanf("%s",infix);
 	convert(infix,postfix);
-	printf("\nPOSFIX EXPRESSION\n%s",postfix);
+	printf("\n\nPOSTFIX EXPRESSION\n%s",postfix);
 	return 0;
 }
 
@@ -26,50 +30,13 @@ void convert(char in[],char post[])
 	char opr;
 	stack s1;
 	int j=0,i;
-	for(i=0;in[i]!='\0';i++)
+	s1.top=-1;
+	for(i=0;in[i]!='\0';i++) 
 	{
 		if(isalpha(in[i]))
-		post[j++]=in[i];
+			post[j++]=in[i];
 		if(in[i]=='(')
-		push(&s1,in[i]);
-		if(in[i]=='+'||in[i]=='-'||in[i]=='/'||in[i]=='*')
-		{
-			if(s1.top!=-1)
-			{
-				opr=pop(&s1);#include<stdio.h>
-#include<ctype.h>
-
-typedef struct conversion
-{
-	char[30];
-	int top;
-}stack;
-
-void push(stack*,char);
-char pop(stack*);
-void convert(char[],char[]);
-int priority(char);
-int main()
-{
-	char infix[30],poxtfix[30];
-	printf("\n enter infix expression\n");
-	scanf("%s" ,infix);
-	convert(infix,postfix);
-	printf("\nPOSFIX EXPRESSION\n%s",postfix);
-	return 0;
-}
-
-void convert(char in[],char post[])
-{
-	char opr;
-	stack s1;
-	int j=0,i;
-	for(i=0;in[i]!='\0';i++)
-	{
-		if(isalpha(in[i]))
-		post[j++]=in[i];
-		if(in[i]=='(')
-		push(&s1,in[i]);
+			push(&s1,in[i]);
 		if(in[i]=='+'||in[i]=='-'||in[i]=='/'||in[i]=='*')
 		{
 			if(s1.top!=-1)
@@ -77,21 +44,14 @@ void convert(char in[],char post[])
 				opr=pop(&s1);
 				while(priority(opr)>=priority(in[i]))
 				{
-					post[j++]=opr;
-					opr=pop(&s1);
-				}
-				push(&s1,)
-
-				while(priority(opr)>=priority(in[i]))
-				{
-					post[j++]=opr;
-					opr=pop(&s1);
+					 post[j++]=opr;
+					 opr=pop(&s1);
 				}
 				push(&s1,opr);
 				push(&s1,in[i]);
 			}
 			else
-			pushpush(&s1,in[i]);
+				push(&s1,in[i]);
 		}
 		if(in[i]==')')
 		{
@@ -104,13 +64,42 @@ void convert(char in[],char post[])
 		}
 	}
 	while(s1.top!=-1)
-	{
 		post[j++]=pop(&s1);
-		post[j]='\0';
-	}
-	
-int priority(char);
+	post[j]='\0';
+ }
+
+int priority(char c)
 {
-	if(char==$)
-	return 3;
-	else if(
+	 if(c=='$')
+		return 3;
+	 if(c=='/'||c=='*')
+		return 2;
+	 if(c=='+'||c=='-')
+		return 1;
+	 else
+		return 0;
+}
+
+void push(stack *s,char opr)
+{
+	s->top++;
+	s->a[s->top]=opr;
+}
+
+char pop(stack *s)
+{
+	if (s->top==-1)
+	{
+		printf("stack is empty\n");
+		return 0;
+    }
+    else
+    {
+		char data=s->a[s->top--];
+		return data;
+    }
+}
+
+
+
+
