e07b2c0714461442562849ec32e1bb7dc9274b7c
user.email <shahid.the.coder@gmial.com>
2017-08-11 15:30:27 +0530

    Queue implementation

--- stack.c
+++ stack.c
@@ -6,19 +6,19 @@ int stack[MAX_SIZE],top=-1;
 
 int isfull(){
 if(MAX_SIZE==top){
-return 1
+return 1;
 }
 else{
-return-1
+return-1;
 }
 }
 
 int isempty(){
 if(MAX_SIZE==top){
-return 1
+return 1;
 }
 else{
-return -1
+return -1;
 }
 }
 
@@ -26,55 +26,63 @@ int peek(){
 return stack[top];
 }
 
-void push(int e){
-if(!isfull()){
-top++;
-stack[top]=e;
-printf("%d is successfully insrted",e);
-}
-else{
-printf("stack overflow");
-}
-}
-
-void pop(){
-int d;
-if(!isempty()){
- d=top;
- top--;
- printf("%d is successfully deleted",d);
-}
-else{
-printf("stack underflow");
-}
+void push(int e)
+ int e;
+	if(!isfull())
+	{
+	top++;
+	stack[top]=e;
+	printf("%d is successfully insrted",e);
+	}
+	else
+	{
+		printf("stack overflow");
+	}
 }
 
-int main(){
-	int choice;e;
+void pop()
+{
+	int d;
+	if(!isempty())
+	{
+		d=top;
+		top--;
+		printf("%d is successfully deleted",d);
+	}
+	else
+	{
+	printf("stack underflow");
+	}
+}
+
+int main()	
+{
+	int choice,e;
 	do{
 		printf(" enter your choice:\n 1.peek\n 2.push\n 3.pop\n 4.exit\n");
-		scanf("%d",&choice");
-		switch(choice){
-			case 1: peek()
+		scanf("%d",&choice);
+		switch(choice)
+		{
+			case 1: 	e=peek();
+						printf("\nElement at the top of stack is : %d",e);
 					break;
 			case 2:
-					push()
+					printf("\nEnter the elment to be inserted: ");
+				scanf("%d",&e);
+				push(e);//call push function
+					push();
 					break;
 			case 3:
-					pop()
+					pop();
 					break;
 			case 4:
-					return 0
+					exit(0);
 					break;
 			default:
 					printf("invalid choice");
 					break;
-		}while(1);
+		}
+	}while(1);
+	return 0;
+}
 					
-		
-
-	
-
-
-
-
