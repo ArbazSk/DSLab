ee78198b26e104f8f3bd6f951911fd2e623ccd01
ArbazSk <arbazshaikh5138@gmail.com>
2017-08-11 20:41:18 +0530

    stack imlementation

--- stack.c
+++ stack.c
@@ -4,39 +4,38 @@
 
 int stack[MAX_SIZE],top=-1;
 
-int isfull(){
-if(MAX_SIZE==top){
+int isfull()
+{
+if(MAX_SIZE==top)
 return 1;
-}
-else{
+else
 return-1;
 }
-}
 
-int isempty(){
-if(MAX_SIZE==top){
+int isempty()
+{
+if(MAX_SIZE==top)
 return 1;
-}
-else{
+else
 return -1;
 }
-}
 
-int peek(){
+int peek()
+{
 return stack[top];
 }
 
 void push(int e)
- int e;
-	if(!isfull())
+{
+    if(!isfull())
 	{
 	top++;
 	stack[top]=e;
-	printf("%d is successfully insrted",e);
+	printf("\n%d is successfully inserted",e);
 	}
 	else
 	{
-		printf("stack overflow");
+		printf("\nElement cannot be inserted. stack overflow");
 	}
 }
 
@@ -45,44 +44,43 @@ void pop()
 	int d;
 	if(!isempty())
 	{
-		d=top;
+		d=stack[top];
 		top--;
-		printf("%d is successfully deleted",d);
+		printf("\n%d is successfully deleted",d);
 	}
 	else
 	{
-	printf("stack underflow");
+	printf("\nstack underflow");
 	}
 }
 
-int main()	
+void main()
 {
 	int choice,e;
 	do{
-		printf(" enter your choice:\n 1.peek\n 2.push\n 3.pop\n 4.exit\n");
+        printf("\n--------------------------------------------\n");
+		printf("\nEnter your choice:\n 1.PEEK\n 2.PUSH\n 3.POP\n 4.EXIT\n");
+		printf("--------------------------------------------\n");
 		scanf("%d",&choice);
 		switch(choice)
 		{
-			case 1: 	e=peek();
-						printf("\nElement at the top of stack is : %d",e);
-					break;
+			case 1: e=peek();
+					printf("\nElement at the top of stack is : %d",e);
+                    break;
 			case 2:
-					printf("\nEnter the elment to be inserted: ");
-				scanf("%d",&e);
-				push(e);//call push function
-					push();
+					printf("\nEnter the element to be inserted: ");
+                    scanf("%d",&e);
+                    push(e);//call push function
 					break;
 			case 3:
 					pop();
 					break;
 			case 4:
 					exit(0);
-					break;
 			default:
-					printf("invalid choice");
+					printf("\nINVALID CHOICE\n");
 					break;
 		}
 	}while(1);
-	return 0;
 }
-					
+
